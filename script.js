be4de693dd56de11aa544f8974a0ad5824bb4ad1
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2019-12-11 23:17:02 -0800

    wrote functions for generating password and copying to clipboard

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
-var specialChar = "!@#$%^&*()";
-var numbersChar = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
-var passwordLength;
-do {
-    var numberOfCharacters = prompt('how many characters would you like your password to contain?');
-    passwordLength = parseInt(numberOfCharacters);
-    if (passwordLength < 8) {
-        alert('password must be 8 or more characters!');
+function generatePassword () {
+    var specialCharacters = "!@#$%^&*()";
+    var numbersCharacters = "0123456789";
+    var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength;
+    do {
+        var numberOfCharacters = prompt('how many characters would you like your password to contain?');
+        passwordLength = parseInt(numberOfCharacters);
+        if (passwordLength < 8) {
+            alert('password must be 8 or more characters!');
+        }
+        if (passwordLength > 128) {
+            alert('password must be 128 or less characters!');
+        }
+    } while (passwordLength < 8 || passwordLength > 128);
+    
+    var hasSpecialCharacters;
+    var hasNumberCharacters;
+    var hasLowerCaseCharacters;
+    var hasUpperCaseCharacters;
+    
+    do {
+        hasSpecialCharacters = confirm('would you like your password to contain special characters?');
+        hasNumberCharacters = confirm('would you like your password to contain numerical characters?');
+        hasLowerCaseCharacters = confirm('would you like your password to contain lowercase characters?');
+        hasUpperCaseCharacters = confirm('would you like your password to contain uppercase characters?');
+    
+        if (!hasSpecialCharacters && !hasNumberCharacters && !hasLowerCaseCharacters && !hasUpperCaseCharacters) {
+            alert('must select at least one character type!');
+        }
+    } while (!hasSpecialCharacters && !hasNumberCharacters && !hasLowerCaseCharacters && !hasUpperCaseCharacters);
+    
+    var selectableCharacters = '';
+    if (hasSpecialCharacters === true) {
+        selectableCharacters += specialCharacters;
     }
-    if (passwordLength > 128) {
-        alert('password must be 128 or less characters!');
+    if (hasNumberCharacters === true) {
+        selectableCharacters += numbersCharacters;
     }
-} while (passwordLength < 8 || passwordLength > 128);
-
-var isSpecialChar;
-var isNumChar;
-var isLowerCase;
-var isUpperCase;
-
-do {
-    isSpecialChar = confirm('would you like your password to contain special characters?');
-    isNumChar = confirm('would you like your password to contain numerical characters?');
-    isLowerCase = confirm('would you like your password to contain lowercase characters?');
-    isUpperCase = confirm('would you like your password to contain uppercase characters?');
-
-    if (!isSpecialChar && !isNumChar && !isLowerCase && !isUpperCase) {
-        alert('must select at least one character type!');
+    if (hasLowerCaseCharacters === true) {
+        selectableCharacters += lowerCaseCharacters;
     }
-} while (!isSpecialChar && !isNumChar && !isLowerCase && !isUpperCase);
+    if (hasUpperCaseCharacters === true) {
+        selectableCharacters += upperCaseCharacters;
+    }
+    
+    var password = '';
+    for (var i = 1; i <= passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random () * selectableCharacters.length);
+        password = password + selectableCharacters.charAt(randomIndex);
+    }
+    
+    document.getElementById('msg').textContent = password;
+}
+
+function copyToClipboard() {
+    var passowrdText = document.getElementById('msg');
+    passowrdText.select();
+    passowrdText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert('Your password ' + passowrdText.value + 'has been copied to the clipboard!');
+}
+
+
